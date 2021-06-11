18c3e30966d9d7d51ab82b5bd25d2f0f09d988e4
aushwin <aushwin7biju@gmail.com>
2021-06-11 11:23:40 +0530

    added octokit core for requests

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
-const Conf = require('conf')
-const config = new Conf()
-
-const inquirer = require('./lib/inquirer')
+const {Octokit} = require('@octokit/core')
 const github =require('./lib/github')
+
+
 const run = async() => {
     let token = await github.getStoredGithubToken()
     if(!token) {
         token = await github.getPersonalAccessToken()
     }
+    const octokit = new Octokit({ auth: token });
+    await octokit.request('GET /user/repos').then((value)=>console.log(value))
     console.log(`Token is\n${JSON.stringify(token)}`)
-
 }
 run ()
